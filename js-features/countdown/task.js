1e764a1c2e0f0a2033d93197dcececfb048a1724
Владимир Ковалёв <v.a.kovalev@list.ru>
2023-09-18 01:17:03 +0300

    Таймер

---
+++ js-features/countdown/task.js
@@ -0,0 +1,8 @@
+const timer = document.getElementById("timer");
+intervalId = setInterval(() => {
+  timer.textContent--;
+  if(timer.textContent == 0) {
+    clearInterval(intervalId);
+    return alert("Вы победили в конкурсе!");
+  }
+}, 1000);
