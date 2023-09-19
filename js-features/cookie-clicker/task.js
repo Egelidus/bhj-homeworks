40ec5c16b2363b92b507de4ccb87804c79b51785
Владимир Ковалёв <v.a.kovalev@list.ru>
2023-09-19 19:53:44 +0300

    mole

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,12 +1,7 @@
 const clicker = document.getElementById("cookie");
-const clickerCounter = document.getElementById("clicker__counter")
-clicker.onclick = function() {
-  clicker.width = 230;
-  clickerCounter.textContent++;
-}
-// clicker.onclick = null;
-// clicker.onclick = function() {
-//   clicker.width = 200;
-//   clickerCounter.textContent++;
-// }
-// clicker.onclick = null;
+const clickerCounter = document.getElementById("clicker__counter");
+clicker.onclick = function () {
+  clickerCounter.textContent++ % 2 == 0
+    ? clicker.width = 230
+    : clicker.width = 200;
+};
--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,21 +1,34 @@
 const getHole = (index) => document.getElementById(`hole${index}`);
+// const moleClass = getHole.className;
 const win = document.getElementById("dead");
 const loss = document.getElementById("lost");
-for (let i = 0; i < 9; i++) {
-  getHole(i).onclick = function () {
-    if (getHole(i).className.includes("hole_has-mole")) {
-      win.textContent++;
-    } else {
-      loss.textContent++;
-    }
-  };
-}
-if (win.textContent == 10) {
+document.getElementById("hole1").onclick = function () {
+  if (document.getElementById("hole1").className == "hole_has-mole") {
+    win.textContent++;
+  } else {
+    loss.textContent++;
+  }
+};
+
+// getHole(1).onclick = function () {
+//   if (getHole(1).className == "hole_has-mole") {
+//     win.textContent++;
+//   } else {
+//     loss.textContent++;
+//   }
+// };
+
+// }
+// for (let i = 0; i < 9; i++) {
+//   getHole(i).onclick = function () {
+// }
+
+if (win.textContent === 10) {
   win.textContent = 0;
   loss.textContent = 0;
   alert("Победа");
 }
-if (loss.textContent == 5) {
+if (loss.textContent > 5) {
   win.textContent = 0;
   loss.textContent = 0;
   alert("Поражение");
